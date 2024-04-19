f16e99f396f764042a8ef8845f34394293d0fcee
HYUNSU <kwenhs0525@naver.com>
2024-04-19 16:37:51 +0900

    "Feat : data.sql 주석 해제"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,16 +4,8 @@ CREATE TABLE IF NOT EXISTS article (
     content varchar(255) not null
 );
 
-CREATE TABLE comment(
-    id BIGINT AUTO_INCREMENT primary key,
-    body VARCHAR(1000) NOT NULL
-);
-
-INSERT INTO article (title, content, created_at, updated_at) VALUES('제목1', '내용1', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES('제목2', '내용2', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES('제목3', '내용3', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES('제목4', '내용4', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES('제목5', '내용5', NOW(), NOW());
-
-INSERT INTO comment (id, body, created_at, updated_at) VALUES(1, '댓글1', NOW(), NOW());
-INSERT INTO comment (id, body, created_at, updated_at) VALUES(2, '댓글2', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES('title1', 'contents1', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES('title2', 'contents2', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES('title3', 'contents3', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES('title4', 'contents4', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES('title5', 'contents5', NOW(), NOW());
